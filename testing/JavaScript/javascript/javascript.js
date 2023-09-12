db4ba25e17a1666a2dcb71220c4a806048d70db6
Gala107 <wgalina@hotmail.com>
2023-09-12 21:09:14 +0000

    2 versions of javascript implementation

---
+++ testing/JavaScript/javascript/javascript.js
@@ -0,0 +1,14 @@
+// function changeParagraph(textColor) {
+//     paragraph.style.color = textColor;
+//     sentence.style.color = textColor;
+// }
+function changeParagraph(textColor) {
+    document.getElementById("paragraph").style.color = textColor;
+    document.getElementById("sentence").style.color = textColor;
+}
+function changeSentence() {
+    document.getElementById("sentence").style.color = "blue";    
+}
+function thankCustomer() {
+    alert("Thank you for reading the paragraph!");
+}
