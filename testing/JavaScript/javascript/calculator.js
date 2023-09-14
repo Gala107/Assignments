afb29fae4fe3076f21434a43d5c30e2b63194c99
Gala107 <wgalina@hotmail.com>
2023-09-14 16:38:40 -0500

    updated css for calculator

--- testing/JavaScript/javascript/calculator.js
+++ testing/JavaScript/javascript/calculator.js
@@ -1,26 +1,28 @@
 function compute(operation){
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
-
     if (num1 == "" || num2 == "") {
         alert("Both numbers have to be entered.\nThank you!");
         return;
     }
 
+    let resultElem = document.getElementById("result");
     if ("add" == operation){
-        document.getElementById("result").value = +num1 + +num2;
+        resultElem.value = +num1 + +num2;
     } else if ("sub" == operation){
-        document.getElementById("result").value = num1 - num2;
+        resultElem.value = num1 - num2;
     } else if ("mul" == operation){
-        document.getElementById("result").value = num1 * num2;
+        resultElem.value = num1 * num2;
     } else if ("div" == operation){
-        document.getElementById("result").value = num1 / num2;
+        resultElem.value = num1 / num2;
     }
 
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
 
-function clearResult(){
+function reset(){
     document.getElementById("result").value = "";
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
 }
